b58ffee538b05ecebcd8ef4238074d1d820eb7cf
feiyie <feiyie@example.com>
2026-09-21 11:29:50 +0800

    Add student  input feature

--- student.py
+++ student.py
@@ -1,4 +1,7 @@
 def show_student():
-    print("Student Score System")
-if __name__ == '__main__':
+    # 学号录入
+    student_id = input("请输入学生学号：")
+    print(f"录入成功！学生学号：{student_id}")
+
+if __name__ == "__main__":
     show_student()
